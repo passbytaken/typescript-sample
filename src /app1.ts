21c2f304538c1e7189fa7ae1c24cffe980b0d95e
Matrix <jakoo@MatrixdeMacBook-Pro-2.local>
2019-03-11 16:53:39 +0800

    initial

--- src /app1.ts
+++ src /app1.ts
@@ -168,4 +168,21 @@ function same(x: any, y?: any): void {
 same('abc')
 same(1, 'xyz')
 
+var month = 0;
+if ( month <= 0 || month > 12) {
+    month = Number.NaN
+    console.log("月份是： " + month)
+} else {
+    console.log("月份数值正确")
+}
 
+function employee(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+}
+var emp = new employee(123, "admin")
+employee.prototype.email = 'admin@ele.me'
+
+console.log('员工号：' + emp.id)
+console.log('员工姓名：' + emp.name)
+console.log('员工邮箱：' + emp.email)
